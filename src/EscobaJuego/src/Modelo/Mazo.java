70373374612d29a7a42cab6cd1a8e89973d8463b
LeonelCamposM <leonelscamposm@gmail.com>
2022-06-20 21:09:43 -0600

    mazo agregado

--- src/EscobaJuego/src/Modelo/Mazo.java
+++ src/EscobaJuego/src/Modelo/Mazo.java
@@ -1,26 +1,57 @@
 package Modelo;
-
+import java.util.Collections;
 import java.util.ArrayList;
 
 public class Mazo {
-    private ArrayList<Naipe> mazo;
-    private int cantidadDeCartas;
+    private ArrayList<Naipe> mazo = new ArrayList<Naipe>();
+    ArrayList<String> palos; 
 
-    public Mazo(){}
+    public Mazo(){
+        palos = new ArrayList<String>() {
+            {
+                add("Copas");
+                add("Espadas");
+                add("Bastos");
+                add("Oros");
+            }
+        };
+
+        for (String palo : palos) {
+            for (int valor = 1; valor < 11; valor++) {
+                mazo.add(new Naipe(valor, palo));
+            }
+        }
+    }
 
     public ArrayList<Naipe> obtenerMazo(){
-        return null;
+        return mazo;
     }
 
     public int obtenerCantidadDeNaipes(){
-        return 0;
+        return mazo.size();
     }
 
     public ArrayList<Naipe> repartirMazo(int cantidadDeNaipes){
-        return null;
+        int cartasElegidas = 0;
+        ArrayList<Naipe> cartas =  new ArrayList<Naipe>();
+        barajar();
+        for (Naipe naipe : mazo) {
+            cartas.add(naipe);
+            cartasElegidas++;
+            if(cartasElegidas == cantidadDeNaipes){
+                break;
+            }
+        }
+        return cartas;
     }
 
     public void barajar(){
+        Collections.shuffle(mazo);
+    }
 
+    public void imprimirMazo(){
+        for (Naipe naipe : mazo) {
+            System.out.println("Palo: "+naipe.obtenerPalo()+" Valor: "+naipe.obtenerValor());
+        }
     }
 }
