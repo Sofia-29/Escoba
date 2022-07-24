b1569b362a11c00a7e2ab70de197dabe0b6d9db6
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-07-24 11:53:15 -0600

    Se arreglaron métodos según comentarios de corrección

--- srcMarda/Marda/src/Modelo/MesaEscoba.java
+++ srcMarda/Marda/src/Modelo/MesaEscoba.java
@@ -11,25 +11,65 @@ class MesaEscoba extends Mesa {
         }
     }
 
-    public ArrayList<Naipe> movimientoJugadorCapturarCarta(Naipe naipe, String nombreJugador){
-        Naipe cartaDescartada = null;
+    @Override
+    protected ArrayList<Carta> movimientoJugadorCapturarCarta(Carta naipe, String nombreJugador){
+        Carta cartaDescartada = null;
         cartaDescartada = movimientoJugadorDescartarCarta(naipe, nombreJugador);
-        ArrayList<Naipe> naipesCapturados = null;
+        ArrayList<Carta> naipesCapturados = null;
+        /* 
         naipesCapturados = validar.validarCaptura(jugadorActual, cartaDescartada, cartasEnMesa);
         if(naipesCapturados != null){
             ultimoJugadorCaptura = jugadorActual;
         }
+        */
         return naipesCapturados;
     }
+    
+    @Override
+    protected Boolean validarRepartirCartas() {
+        if(primerJugador.obtenerNumeroCartasEnJuego()==0 && segundoJugador.obtenerNumeroCartasEnJuego()==0){
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
 
-    public Naipe movimientoJugadorDescartarCarta(Naipe naipe, String nombreJugador){
-        ArrayList<Naipe> naipes = new ArrayList<Naipe>();
-        Naipe cartaDescartada = naipe;
+    @Override
+    protected Boolean validarTerminarPartida() {
+        //if(mazo.obtenerCantidadDeNaipes() == 0 && primerJugador.obtenerNumeroCartasEnJuego() == 0 && segundoJugador.obtenerNumeroCartasEnJuego() == 0){
+            //ultimoJugadorCaptura.capturarCartas(cartasEnMesa);
+            cartasEnMesa.removeAll(cartasEnMesa);
+            return true;
+        //}
+        //return false;
+    }
+
+    @Override
+    protected Jugador declararGanador() {
+        int puntajePrimerJugador = primerJugador.obtenerPuntaje();
+        int puntajeSegundoJugador = segundoJugador.obtenerPuntaje();
+        if(puntajePrimerJugador>puntajeSegundoJugador){
+            return primerJugador;
+        }else{
+            if(puntajeSegundoJugador>puntajePrimerJugador){
+                return segundoJugador;
+            }else{
+                return null;
+            }
+        }
+    }
+
+    @Override
+    protected Carta movimientoJugadorDescartarCarta(Carta naipe, String nombreJugador) {
+        ArrayList<Carta> naipes = new ArrayList<Carta>();
+        Carta cartaDescartada = naipe;
         if(jugadorActual.obtenerNombre() == obtenerJugadorPersona(nombreJugador).obtenerNombre()){
             naipes.add(naipe);
             cartaDescartada = jugadorActual.descartarCarta(naipes);
         }
-        jugadorActual.removerCarta(cartaDescartada);
+        //*jugadorActual.removerCarta(cartaDescartada);
         return cartaDescartada;
     }
 }
