46cb0af30a097499d50036df805f30de4f40244b
Jeremy Vargas Artavia <jeremychaman@gmail.com>
2022-07-28 18:29:53 -0600

    Borré un método no necesario.

--- srcMarda/Marda/src/Modelo/Jugador.java
+++ srcMarda/Marda/src/Modelo/Jugador.java
@@ -28,10 +28,6 @@ public abstract class Jugador {
         this.cartas = cartas;
     }
 
-    public void asignarCartasCapturadas(ArrayList<Carta> cartas){
-        this.cartasCapturadas = cartas;
-    }
-
     public void asignarPuntaje(int puntaje){
         this.puntaje += puntaje;
     }
